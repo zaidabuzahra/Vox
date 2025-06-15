f727141579d892a3616e4c1c7685dbed98a48f63
zaidabuzahra <73735421+zaidabuzahra@users.noreply.github.com>
2025-06-16 01:59:10 +0300

    fixing UI

--- Assets/TriggerOnKeyPress.cs
+++ Assets/TriggerOnKeyPress.cs
@@ -3,13 +3,22 @@ using UnityEngine.Events;
 
 public class TriggerOnKeyPress : MonoBehaviour
 {
+    public bool Q, Shift, Click;
     public UnityEvent onKeyPressQ;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.R))
+        if ((Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.R) ) && Q)
         {
             onKeyPressQ?.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Shift)
+        {
+            gameObject.SetActive(false);
+        }
+        if (Input.GetMouseButtonDown(0) && Click)
+        {
+            gameObject.SetActive(false);
         }
     }
 }
